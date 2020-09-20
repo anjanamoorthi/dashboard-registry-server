a84e7bbc2a80333b27a14385d67fc68a9d6a6d30
Moorthi <Moorthi.Rajan@invenco.com>
2020-09-21 10:10:10 +1200

    Hack-42 Init microservice for Eureka Server

---
+++ src/test/java/com/invenco/dashboardRegistry/DashboardRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenco.dashboardRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
